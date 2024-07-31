febc3fb4361d732ba568ecd691195105b76c14fe
shokouhi62 <hamed.shokouhi62@gmail.com>
2024-07-31 11:43:48 +0330

    Create ackley.m

---
+++ ackley.m
@@ -0,0 +1,13 @@
+function ObjVal = ackley(Chrom,switch1);
+
+Dim=size(Chrom,2);
+   
+    [Nind,Nvar] = size(Chrom);
+
+
+      A = 1/Dim;
+      Omega = 2 * pi;
+      sum1=A.*sum((Chrom .* Chrom)')';
+       
+      sum2=A.*sum((cos(Omega * Chrom))')';
+      ObjVal = -20*exp(-0.2*sqrt(sum1))-exp(sum2)+20+exp(1);
