01d71eb682fb25e36f03a97873672c9553fcbd0c
shokouhi62 <hamed.shokouhi62@gmail.com>
2024-07-31 11:53:14 +0330

    Create indata.m

---
+++ indata.m
@@ -0,0 +1,15 @@
+%read dataset
+data=xlsread('dataset_sample.xlsx'); 
+graph_T162_sub1=1;
+graph_T145_sub196=1;
+graph_T145_sub0=1;
+graph_T144_sub27=1;
+graph_T144_sub6=1;
+graph_T144_sub4=1;
+graph_T131_sub0=1;
+graph_T107_sub34=1;
+graph_T107_sub1=1;
+graph_T75_sub5=1;
+graph_T75_sub0=1;
+graph_T24_sub0=1;
+graph_T16_sub0=1;
